da094b0d2e1b1f413ae767899c1f3a795ccbe451
Gopihanuma <gopihanuma.ponnam@gmail.com>
2025-04-30 16:35:23 +0530

    kops.sh

--- kops.sh
+++ kops.sh
@@ -2,7 +2,10 @@
 #vim .bashrc
 #export PATH=$PATH:/usr/local/bin/
 #source .bashrc
-
+vi .bashrc    #command
+  export PATH=$PATH:/usr/local/bin/
+  :wq!
+source .bashrc    #command
 
 #! /bin/bash
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
@@ -11,10 +14,12 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket reyaz-kops-testbkt123.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
-aws s3api put-bucket-versioning --bucket reyaz-kops-testbkt123.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://reyaz-kops-testbkt123.k8s.local
-kops create cluster --name reyaz.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
-kops update cluster --name reyaz.k8s.local --yes --admin
+aws s3api create-bucket --bucket gopi-kops-testbkt123.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
+aws s3api put-bucket-versioning --bucket gopi-kops-testbkt123.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://gopi-kops-testbkt123.k8s.local
+kops create cluster --name gopi.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+kops update cluster --name gopi.k8s.local --yes --admin
 
-#kops validate cluster --wait 10m
+kops update cluster     #command---> cluster-name, --yes, --admin
+
+kops validate cluster --wait 10m     #command
